a54aacb180816e8d8390730a03e483db52c63953
IVANILDA TEREZINHA DE OLIVEIRA <ivanilda.toliveira@senacsp.edu.br>
2024-05-10 20:24:57 -0300

    atualiozada

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,20 +1,22 @@
-import { View, Text } from 'react-native';
-import { styles } from './styles'
+import {View, Text} from 'react-native';
+import { styles } from './styles';
+import { Ingredient } from '@/components/ingredient';
 
-export default function Home() {
+export default function Home(){
 
-    return (
+    return(
         <View style={styles.container}>
-
             <Text style={styles.title}>
-                Escolha{"\n"}
-                <Text style={styles.subtitle}>os produtos</Text>
-            </Text>
-            <Text style={styles.message}>
-                Descubra receitas baseadas nos produtos que voce escolheu.
-            </Text>
+                Escolha {"\n"}
+                <Text>os produtos</Text>
+                </Text>
+                <Text style={styles.message}>
+                    Descubra receitas baseadas nos produtos que você escolheu.
+                </Text>
+                <Ingredient/>
+                <Ingredient/>
+                <Ingredient/>
+                <Ingredient/>
         </View>
-
     )
 }
-
