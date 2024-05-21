d057da50c2f28be8dc189ad49ed743fdbc02d21c
IVANILDA TEREZINHA DE OLIVEIRA <ivanilda.toliveira@senacsp.edu.br>
2024-05-20 22:08:53 -0300

    my-app-cook4

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,19 +1,31 @@
-import { View, Text, ScrollView } from 'react-native';
+//*verificar os import para saber quais componentes tem no programa*/ 
+
+import { View, Text, ScrollView, Alert } from 'react-native';
 import { styles } from './styles'
 import { Ingredient } from '@/components/ingredient';
 import { useState } from 'react';
+import { Selected } from '@/components/selected';
 
 export default function Home() {
     const [selected, setSelected] = useState<string[]>([])
 
-    function handleToggleSelected(value: string ) {
-       // console.log(value);
-        if(selected.includes(value)){
+    function handleToggleSelected(value: string) {
+        // console.log(value);
+        if (selected.includes(value)) {
             return setSelected((state) => state.filter((item) => item !== value))
         }
         setSelected((state) => [...state, value])
-      //  console.log(selected);
+        //  console.log(selected);
 
+    }
+        /*função para limpa a seleção dos itens*/ 
+
+    function handleClearSeleted(){
+        Alert.alert("Limpar", "Deseja limpa tudo?",[
+            {text: "Não", style: "cancel"},
+            {text: "Sim", onPress:() => setSelected([])}
+        ])
+       
     }
 
     return (
@@ -23,6 +35,7 @@ export default function Home() {
                 Escolha{"\n"}
                 <Text style={styles.subtitle}>os produtos</Text>
             </Text>
+
             <Text style={styles.message}>
                 Descubra receitas baseadas nos produtos que voce escolheu.
             </Text>
@@ -33,13 +46,23 @@ export default function Home() {
 
                 contentContainerStyle={styles.ingredients}
                 showsVerticalScrollIndicator={false}
-
             >
-
                 {Array.from({ length: 100 }).map((item, index) => (
-                    <Ingredient key={index} name="Morango" image="" selected={selected.includes(String(index))} onPress={() => handleToggleSelected(String(index))} />))}
+                    <Ingredient key={index} name="Maça" image="" selected={selected.includes(String(index))} onPress={() => handleToggleSelected(String(index))} />))}
 
             </ScrollView>
+
+            {selected.length > 0 &&
+
+                <Selected 
+                    quantity={selected.length}
+                    onClear={handleClearSeleted}
+                    onSearch={() =>{}}
+                
+                />
+
+            }
+                    
 
         </View>
 
--- src/components/selected/index.tsx
+++ src/components/selected/index.tsx
@@ -2,22 +2,41 @@ import { View, Text } from "react-native"
 import Animated, { SlideInDown, BounceOutDown } from "react-native-reanimated"
 import { MaterialIcons } from "@expo/vector-icons"
 
+
 import { styles } from './styles'
 import { theme } from '@/theme'
+import { Button } from "../Button"
 
-export function Selected() {
+type Props = {
+    quantity: number
+    onClear: () => void
+    onSearch: () => void
+}
+export function Selected({ quantity, onClear, onSearch }: Props) {
+
+
+
+    
+/*um componete dentro de outro componete (Animated e Button) e o icone MaterialIcons*/ 
+
+
     return (
-        <Animated.View style={styles.container}>
+        <Animated.View style={styles.container}
+            entering={SlideInDown.duration(500)}
+            exiting={BounceOutDown}>
+                
             <View style={styles.header}>
-                <Text style={styles.label}>?? ingredientes selecionados</Text>
+
+                <Text style={styles.label}>{quantity} ingredientes selecionados</Text>
+
                 <MaterialIcons
                     name="close"
                     size={24}
+                    onPress={onClear}
                     color={theme.colors.gray_400}
-
-
                 />
             </View>
+            <Button title="Encontar" onPress={onSearch} />
 
         </Animated.View>
     )
